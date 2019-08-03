25772945c2669518f0855b31dfd0b7783a2a5bbe
Good-Bird <goodborb@gmail.com>
2019-08-02 22:26:37 -0400

    comments

--- I7koDdpzHJ/Assets/Scripts/BatteryPowerInteractions.cs
+++ I7koDdpzHJ/Assets/Scripts/BatteryPowerInteractions.cs
@@ -4,44 +4,53 @@ using UnityEngine;
 
 public class BatteryPowerInteractions : MonoBehaviour
 {
-    [SerializeField] private float power, maxPower, powerRechargeAmount, powerRechargeRate, powerTransferRate;
+    //Power is the amount of power in the battery currently, maxpower is the amount the battery can hold, powerRechargeAmount is the amount of power the battery passivly recives per interval, 
+    //powerRechargeRate how often power is recived, powerTransferAmount how much power is given up to whatever it is plugged into, percentMaxStart the percent of the max that the battery starts with
+    [SerializeField] private float power, maxPower, powerRechargeAmount, powerRechargeRate, powerTransferAmount, percentMaxStart;
+    //is the battery charging
     [SerializeField] private bool charging;
+    //whatever object the battery is connected to
     private GameObject connectedTo;
 
     private void Start()
     {
-        power = maxPower * 0.5f;
+        power = maxPower * percentMaxStart;
     }
 
     private void Update()
     {
-        /*
-        if(connectedTo.tag.Equals("BatteryStation") && !charging)
+        //determines when recharge
+        if(connectedTo != null && connectedTo.tag.Equals("BatteryStation") && !charging)
         {
             charging = true;
             Invoke("charge", powerRechargeRate);
         }
-        */
+        
     }
 
+    //adds amount to the total power
     private void changePower(float amount)
     {
         power += amount;
     }
 
+    //sets power to desired number
     public void setPower(float amount)
     {
         power = amount;
     }
     
+    //returns power
     public float getPower() { return power; }
 
+    //returns what machine the plug is connected to
     public ResourceSystem getConnectedMachine()
     {
         return new ResourceSystem();
         return null;
     }
 
+    //charging the battery
     public bool charge()
     {
         if (power < maxPower)
@@ -54,16 +63,18 @@ public class BatteryPowerInteractions : MonoBehaviour
         return false;
     }
 
+    //returns the drained power so whatever is draining it can take it
     public float drainPower()
     {
         if(power > 0.0f)
         {
-            power -= powerTransferRate;
-            return powerTransferRate;
+            power -= powerTransferAmount;
+            return powerTransferAmount;
         }
         return 0;
     }
 
+    //returns true if battery is connected to something
     public bool batteryConnected()
     {
         return connectedTo != null;
--- I7koDdpzHJ/Assets/Scripts/ResourceSystem.cs
+++ I7koDdpzHJ/Assets/Scripts/ResourceSystem.cs
@@ -4,39 +4,47 @@ using UnityEngine;
 
 public class ResourceSystem : MonoBehaviour
 {
-    [SerializeField] private float resource, maxResources, lossAmount, lossRate, gainRate;
+    // resources is the amount of any given resource a machine has, maxResources is the machines max capacaity, lossAmount is amount lost per interval, lossRate is how often energy is lost, gainRate is how often energy is gained, starting resources is what percent of the max is started with
+    [SerializeField] private float resource, maxResources, lossAmount, lossRate, gainRate, startingResources;
+    //broken determines wether or not the machine is functioning or requires fixing, gainingPower and LosingPower are purly logical operators
     [SerializeField] private bool broken, gainingPower, losingPower;
+    //contains the script for the plug
     [SerializeField] private BatteryPowerInteractions plugInfo;
+    //contains the plug game object
     [SerializeField] public GameObject plug;
 
     private void Start()
     {
-        resource = maxResources * 0.5f;
+        resource = maxResources * startingResources;
     }
     private void Update()
     {
+        //starts losing power when there is no plug
         if(!hasPlug() && !losingPower)
         {
             losingPower = true;
             Invoke("loseResources", lossRate);
         }
+        //begins gaining power when there is a plug
         else if(!gainingPower)
         {
             gainingPower = true;
             Invoke("gainResources", gainRate);
         }
-
+        //if the plug object is attached the plug script is initaliezed on plugInfo
         if(hasPlug())
         {
             plugIn();
         }
     }
 
+    //sets the state of the broken variable
     public void setBreak(bool isBroken)
     {
         broken = isBroken;
     }
 
+    //increases the amount of resource a machine has and keeps it from going above max
     public void gainResources()
     {
         if (hasPlug())
@@ -53,6 +61,7 @@ public class ResourceSystem : MonoBehaviour
         gainingPower = false;
     }
 
+    //decreases the amount of resource a machine has and keeps it from going below 0
     public void loseResources()
     {
         if (resource > 0)
@@ -66,11 +75,13 @@ public class ResourceSystem : MonoBehaviour
         losingPower = false;
     }
 
+    //initializes pluginfo to the plugs script
     public void plugIn()
     {
         plugInfo = plug.GetComponent<BatteryPowerInteractions>();
     }
 
+    // determines wheter the plug gameobject is attached
     public bool hasPlug()
     {
         return plug != null;
