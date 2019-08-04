9483f82e51a295124af9311f34899a16994aa7df
PedroDuarte536 <pedro.dld536@gmail.com>
2019-08-04 13:03:08 +0100

    Fixed Display

--- I7koDdpzHJ/Assets/Scripts/PercentageDisplay.cs
+++ I7koDdpzHJ/Assets/Scripts/PercentageDisplay.cs
@@ -24,6 +24,7 @@ public class PercentageDisplay : MonoBehaviour
     private void updateDisplay()
     {
         float newWidth = maxWidth * ((float)percentage / 100);
+        Debug.Log(newWidth);
         Vector2 newSize = new Vector2(newWidth, displaySR.size.y);
         displaySR.size = newSize;
     }
